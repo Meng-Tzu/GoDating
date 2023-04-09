9ebd163faf39cb62a34783c853c0b7fbdfd0fc6c
Meng-Tzu Tsai <e34049030@gs.ncku.edu.tw>
2023-04-10 00:58:15 +0800

    save and send picture by base64 encoding

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 // 導入模組
 import express from "express";
 import { Server } from "socket.io";
-import { writeFile } from "fs";
+import { writeFile, createReadStream } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { uniqueSuffix } from "./util/ramdom.js";
+
+const __filename = fileURLToPath(import.meta.url);
+// console.log(__filename);
+
+const __dirname = path.dirname(__filename);
+// console.log(__dirname);
 
 //創建 express 的物件
 const app = express();
@@ -23,6 +32,9 @@ let count = 0;
 // 如何儲存哪個連線是誰
 let connections = {};
 
+// // 回覆的訊息格式
+// const response = {};
+
 // FIXME: 找到是誰傳訊息 (如何比遍歷更高效率)
 function getKeyByValue(value) {
   return Object.keys(connections).find((key) => connections[key] === value);
@@ -43,38 +55,92 @@ io.on("connection", (socket) => {
   });
 
   socket.on("message", (msg) => {
+    // 回覆的訊息格式
+    const response = {};
     // console.log("msg", msg, typeof msg);
     const message = JSON.parse(msg);
     // console.log("connections", connections);
     let name = getKeyByValue(socket);
-    const response = { name, message: "" };
+    response.name = name;
 
     if (message.receiver === "ALL") {
       response.message = message.content;
-
       // 針對所有連線去傳送訊息
       io.emit("allMessage", response);
     } else if (!(message.receiver in connections)) {
       socket.emit("notExist", `This person is not online.`);
     } else {
       response.message = message.content;
-
       // 針對 receiver 的這條連線去傳送訊息
       connections[message.receiver].emit("message", response);
     }
   });
 
-  // FIXME: 儲存使用者上傳的照片
   socket.on("upload", (file, callback) => {
     console.log(file); // <Buffer 25 50 44 ...>
 
-    // save the content to the disk, for example
-    writeFile("upload/test2.jpg", file, (err) => {
-      callback({ message: err ? "failure" : "success" });
+    // 回覆的訊息格式
+    const response = {};
+
+    let name = getKeyByValue(socket);
+    response.name = name;
+
+    // save the content to the disk
+    let filename = `${uniqueSuffix()}.jpg`; // 自動編號照片名稱
+    // console.log("filename", filename);
+
+    writeFile(`upload/${filename}`, file, (err) => {
+      if (err) {
+        response.error = "system alert -> This picture cannot display.";
+        io.emit("wholeFile", response);
+      } else {
+        console.log("successfully save");
+        // FIXME: 讀取硬碟中的圖片 (解析度跑掉)
+        let readStream = createReadStream(
+          path.join(__dirname, `/upload/${filename}`),
+          {
+            encoding: "binary",
+          }
+        );
+
+        let chunks = [];
+
+        readStream.on("readable", () => {
+          console.log("Image loading");
+          let chunk;
+          while ((chunk = readStream.read()) !== null) {
+            chunks.push(chunk);
+            io.emit("file", chunk);
+          }
+        });
+
+        readStream.on("end", () => {
+          console.log("Image loaded");
+          response.status = "success";
+          io.emit("wholeFile", response);
+        });
+      }
     });
 
-    // TODO: 傳照片到聊天室
+    // FIXME: 直接傳照片到聊天室，不儲存到硬碟 (bug exist)
+    // let name = getKeyByValue(socket);
+    // response.name = name;
+
+    // response.files = file;
+    // io.emit("files", response);
   });
+
+  // FIXME: test whole picture display
+  // socket.on("test", (msg) => {
+  //   console.log(msg);
+  //   // 回覆的訊息格式
+  //   const response = {};
+
+  //   let name = getKeyByValue(socket);
+  //   response.name = name;
+  //   response.status = msg;
+  //   io.emit("wholeFile", response);
+  // });
 
   // FIXME: 監聽 client 是否已經斷開連線 (可做哪個使用者已離開)
   socket.on("disconnect", () => {
@@ -82,14 +148,3 @@ io.on("connection", (socket) => {
     console.log(`One client has disconnected. 目前連線數: ${count}`);
   });
 });
-
-// index.js
-// io.on("connection", function (socket) {
-//   console.log("One client has connected.");
-//   // 建立一個 "sendMessage" 的監聽
-//   socket.on("online", function (name) {
-//     console.log(name);
-//     // 當收到事件的時候，也發送一個 "allMessage" 事件給所有的連線用戶
-//     io.emit("allMessage", `Hello, everyone. I'm ${name}.`);
-//   });
-// });
