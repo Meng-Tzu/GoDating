afd7f4b54e682fd37f7ad0a6c41f762d2f9db02e
Meng-Tzu Tsai <e34049030@gs.ncku.edu.tw>
2023-04-12 02:30:09 +0800

    add fuzzysearch route for ES

--- routes/chat_record_route.js
+++ routes/chat_record_route.js
@@ -2,13 +2,19 @@
 import express from "express";
 const chatRouter = express.Router();
 
-import { saveChatRecord } from "../controllers/chat_record_controller.js";
+import {
+  saveChatRecord,
+  searchChatRecord,
+} from "../controllers/chat_record_controller.js";
 
 // 可使用的 request body 格式
 chatRouter.use(express.json());
 
 // FIXME: 存聊天紀錄到 ES (驗證 JWT)
 chatRouter.post("/chat/chatrecord", saveChatRecord);
+
+// callback function
+chatRouter.get("/chat/fuzzysearch", searchChatRecord);
 
 // 輸出路由
 export { chatRouter };
