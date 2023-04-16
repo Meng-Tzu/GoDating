7976697d1ec2f4c23ab044b4edddcc797dc8f90b
Meng-Tzu Tsai <e34049030@gs.ncku.edu.tw>
2023-04-16 16:15:34 +0800

    create route for chat record

--- controllers/chat_record_controller.js
+++ controllers/chat_record_controller.js
@@ -1,26 +1,52 @@
 import {
-  deleteChatRecordFromElasticsearch,
-  initElasticsearch,
+  deleteChatIndexFromES,
+  initChatIndexOfES,
   saveChatRecordToES,
-  searchAllFromChatting,
-  searchKeywordFromChatting,
+  searchAllChatFromES,
+  searchKeywordFromES,
 } from "../models/chat_record_model.js";
 
+// FIXME: 刪除聊天記錄的 index
+const deleteChatIndex = async (req, res) => {
+  const { indexId } = req.body;
+
+  try {
+    await deleteChatIndexFromES(indexId);
+    const response = { data: `successfully delete chat#${indexId}` };
+    res.json(response);
+  } catch (error) {
+    console.error("delete index error:", error);
+    res.status(500).json({ error: "Cannot delete chat index normally." });
+  }
+};
+
+// FIXME: 創建聊天記錄的 index
+const creatChatIndex = async (req, res) => {
+  const { indexId } = req.body;
+
+  try {
+    await initChatIndexOfES(indexId);
+    const response = { data: `successfully create chat#${indexId}` };
+    res.json(response);
+  } catch (error) {
+    console.error("init index error:", error);
+    res.status(500).json({ error: "Cannot init chat index normally." });
+  }
+};
+
+// FIXME: 存入聊天記錄
 const saveChatRecord = async (req, res) => {
-  const userId = +req.body.userId;
-  const userName = req.body.userName;
-  const message = req.body.message;
-  const timestamp = req.body.timestamp;
-  const time = +req.body.time;
+  const { indexId, userId, userName, message, timestamp, time } = req.body;
 
   // 存入 ES
   try {
     const record = await saveChatRecordToES(
-      userId,
+      indexId,
+      +userId,
       userName,
       message,
       timestamp,
-      time
+      +time
     );
     const response = { data: record };
     console.log("save into ES successfully");
@@ -29,7 +55,7 @@ const saveChatRecord = async (req, res) => {
   } catch (error) {
     const response = { data: error };
     console.error(
-      "存入 ES 錯誤, check client.index() or client.indices.refresh() :",
+      "save into index error, check client.index() or client.indices.refresh():",
       error
     );
 
@@ -38,24 +64,38 @@ const saveChatRecord = async (req, res) => {
   }
 };
 
-// TODO: 在 elasticsearch 搜尋 products
-const searchChatRecord = async (req, res) => {
-  let keyword = req.query.keyword;
+// FIXME: 載入所有聊天記錄
+const loadAllChatRecord = async (req, res) => {
+  const { indexId } = req.body;
 
   try {
-    if (keyword) {
-      let messages = await searchKeywordFromChatting(keyword);
-      messages = { data: messages };
-      res.json(messages);
-    } else {
-      let messages = await searchAllFromChatting();
-      messages = { data: messages };
-      res.json(messages);
-    }
+    let messages = await searchAllChatFromES(indexId);
+    messages = { data: messages };
+    res.json(messages);
   } catch (error) {
-    console.error("error", error);
-    res.status(500).json({ error: "ElasticSearch cannot work normally." });
+    console.error("load all record error:", error);
+    res.status(500).json({ error: "Cannot load all chat record normally." });
   }
 };
 
-export { saveChatRecord, searchChatRecord };
+// FIXME: 搜尋聊天關鍵字
+const searchChatRecord = async (req, res) => {
+  const { indexId, keyword } = req.body;
+
+  try {
+    let messages = await searchKeywordFromES(indexId, keyword);
+    messages = { data: messages };
+    res.json(messages);
+  } catch (error) {
+    console.error("search certain record error:", error);
+    res.status(500).json({ error: "Cannot search certain record normally." });
+  }
+};
+
+export {
+  deleteChatIndex,
+  creatChatIndex,
+  saveChatRecord,
+  loadAllChatRecord,
+  searchChatRecord,
+};
--- routes/chat_record_route.js
+++ routes/chat_record_route.js
@@ -3,17 +3,28 @@ import express from "express";
 const chatRouter = express.Router();
 
 import {
+  deleteChatIndex,
+  creatChatIndex,
   saveChatRecord,
+  loadAllChatRecord,
   searchChatRecord,
 } from "../controllers/chat_record_controller.js";
 
 // 可使用的 request body 格式
 chatRouter.use(express.json());
 
-// FIXME: 存聊天紀錄到 ES (驗證 JWT)
-chatRouter.post("/chat/chatrecord", saveChatRecord);
+// TODO: 刪除聊天記錄的 index
 
-// callback function
+// FIXME: 創建聊天記錄的 index (驗證 JWT)
+chatRouter.post("/chat/createindex", creatChatIndex);
+
+// FIXME: 存聊天紀錄到 ES (驗證 JWT)
+chatRouter.post("/chat/saverecord", saveChatRecord);
+
+// FIXME: 載入全部聊天紀律 (驗證 JWT)
+chatRouter.get("/chat/allrecord", loadAllChatRecord);
+
+// FIXME: 模糊搜尋聊天紀錄 (驗證 JWT)
 chatRouter.get("/chat/fuzzysearch", searchChatRecord);
 
 // 輸出路由
