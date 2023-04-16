95e676bd4db1075efce302a9d69cc81fcafdad95
Meng-Tzu Tsai <e34049030@gs.ncku.edu.tw>
2023-04-16 19:47:20 +0800

    update list of who_like_me

--- routes/user_route.js
+++ routes/user_route.js
@@ -6,6 +6,7 @@ import { getUserIdName } from "../controllers/user_controller.js";
 import {
   AllUserMatchList,
   certainUserMatchList,
+  certainUserSuitorList,
 } from "../controllers/match_controller.js";
 
 // 可使用的 request body 格式
@@ -19,6 +20,9 @@ userRouter.get("/user/matchcandidate", AllUserMatchList);
 
 // FIXME: 特定使用者的候選人名單 (需要增加權限管理???)
 userRouter.post("/user/candidate", certainUserMatchList);
+
+// FIXME: 特定使用者的候選人名單 (需要增加權限管理)
+userRouter.post("/user/suitor", certainUserSuitorList);
 
 // 輸出路由
 export { userRouter };
