d78eb79f9de18122ab67627357cfed6a68cb4480
Meng-Tzu Tsai <e34049030@gs.ncku.edu.tw>
2023-04-17 14:58:43 +0800

    add Scroll API for searching all data in index

--- models/chat_record_model.js
+++ models/chat_record_model.js
@@ -1,17 +1,13 @@
 import { client } from "../util/util.js";
 
-// FIXME: 清除 chatting index 內的所有資料
-const deleteChatIndexFromES = async (indexId) => {
-  const indexName = indexId;
-
+// 清除 chatting index 內的所有資料
+const deleteChatIndexFromES = async (indexName) => {
   await client.indices.delete({ index: indexName, ignore_unavailable: true });
   console.log(`delete index "${indexName}" successfully`);
 };
 
-// FIXME: 在 elasticsearch 創建 index 並設定規則
-const initChatIndexOfES = async (indexId) => {
-  const indexName = indexId;
-
+// 在 elasticsearch 創建 index 並設定規則
+const initChatIndexOfES = async (indexName) => {
   await client.indices.create({
     index: indexName,
     body: {
@@ -48,17 +44,15 @@ const initChatIndexOfES = async (indexId) => {
   console.log(`init index "${indexName}" successfully`);
 };
 
-// FIXME: 將聊天紀錄存入 ES 中
+// 將聊天紀錄存入 ES 中
 const saveChatRecordToES = async (
-  indexId,
+  indexName,
   userId,
   userName,
   message,
   timestamp,
   time
 ) => {
-  const indexName = indexId;
-
   // 儲存格式
   const result = await client.index({
     index: indexName,
@@ -77,31 +71,57 @@ const saveChatRecordToES = async (
   return result;
 };
 
-// FIXME: 搜尋所有 chatting 內的對話紀錄
-const searchAllChatFromES = async (indexId) => {
-  const indexName = indexId;
-
-  const searchResponse = await client.search({
+// 搜尋所有 chatting 內的對話紀錄 ( Scroll 功能可顯示超過 10 筆資料)
+const searchAllChatFromES = async (indexName) => {
+  const searchParams = {
     index: indexName,
+    scroll: "1m", // 設定 Scroll API 的 scroll 時間
     body: {
       query: {
         match_all: {},
       },
       sort: [{ time: { order: "asc" } }],
+      size: 10,
     },
-  });
+  };
 
-  const result = searchResponse.hits.hits;
-  const messages = result.map((message) => message._source);
+  const searchResponse = await client.search(searchParams);
+  let scrollId = searchResponse._scroll_id;
+
+  // 處理完第一次的搜尋結果， result 變數包含了搜尋結果的資料
+  let initialResult = searchResponse.hits.hits;
+  const messages = initialResult.map((message) => message._source);
+
+  // 使用 scrollId 來繼續滾動搜尋結果
+  while (initialResult.length > 0) {
+    const scrollResponse = await client.scroll({
+      scroll_id: scrollId,
+      scroll: "1m", // 每次滾動都需要指定 scroll 時間
+    });
+
+    const scrollResult = scrollResponse.hits.hits;
+    scrollResult.forEach((message) => messages.push(message._source));
+
+    if (!scrollResult.length) {
+      // 如果滾動後的資料為空，則退出迴圈
+      break;
+    }
+
+    scrollId = scrollResponse._scroll_id;
+    initialResult = scrollResult;
+  }
+
+  // 最後記得清除 Scroll API 上下文
+  await client.clearScroll({ scroll_id: scrollId });
+
   return messages;
 };
 
-// FIXME: 搜尋 elasticSearch 的 chatting 特定關鍵字
-const searchKeywordFromES = async (indexId, keyword) => {
-  const indexName = indexId;
-
-  const searchResponse = await client.search({
+// 搜尋 elasticSearch 的 chatting 特定關鍵字 ( Scroll 功能可顯示超過 10 筆資料)
+const searchKeywordFromES = async (indexName, keyword) => {
+  const searchParams = {
     index: indexName,
+    scroll: "1m", // 設定 Scroll API 的 scroll 時間
     body: {
       query: {
         multi_match: {
@@ -114,11 +134,39 @@ const searchKeywordFromES = async (indexId, keyword) => {
         },
       },
       sort: [{ time: { order: "desc" } }],
+      size: 10,
     },
-  });
+  };
 
-  const result = searchResponse.hits.hits;
-  const messages = result.map((message) => message._source);
+  const searchResponse = await client.search(searchParams);
+  let scrollId = searchResponse._scroll_id;
+
+  // 處理完第一次的搜尋結果， result 變數包含了搜尋結果的資料
+  let initialResult = searchResponse.hits.hits;
+  const messages = initialResult.map((message) => message._source);
+
+  // 使用 scrollId 來繼續滾動搜尋結果
+  while (initialResult.length > 0) {
+    const scrollResponse = await client.scroll({
+      scroll_id: scrollId,
+      scroll: "1m", // 每次滾動都需要指定 scroll 時間
+    });
+
+    const scrollResult = scrollResponse.hits.hits;
+    scrollResult.forEach((message) => messages.push(message._source));
+
+    if (!scrollResult.length) {
+      // 如果滾動後的資料為空，則退出迴圈
+      break;
+    }
+
+    scrollId = scrollResponse._scroll_id;
+    initialResult = scrollResult;
+  }
+
+  // 最後記得清除 Scroll API 上下文
+  await client.clearScroll({ scroll_id: scrollId });
+
   return messages;
 };
 
