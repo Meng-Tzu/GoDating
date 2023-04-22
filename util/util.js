5fde68553f63541792a846fe3da99e196513d17b
Meng-Tzu Tsai <e34049030@gs.ncku.edu.tw>
2023-04-23 01:57:33 +0800

    [feat] upload picture by multer

--- util/util.js
+++ util/util.js
@@ -1,3 +1,6 @@
+import multer from "multer";
+import { v4 as uuidv4 } from "uuid";
+
 // Function1: 計算的年齡
 const getAge = (dateString) => {
   const today = new Date();
@@ -11,4 +14,37 @@ const getAge = (dateString) => {
   return age;
 };
 
-export { getAge };
+// 圖片上傳
+// 設定圖片儲存目的地&檔名
+const storagePicture = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads/");
+  },
+  filename: (req, file, cb) => {
+    const filename = uuidv4();
+    // 取得副檔名
+    const ext = file.originalname.split(".")[1];
+
+    cb(null, `${filename}.${ext}`);
+  },
+});
+
+// 限制上傳檔案的規格
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    return cb(null, true);
+  }
+  cb(new Error("Please upload an image"));
+};
+
+// 圖片上傳設定
+const upload = multer({
+  limit: {
+    // 限制上傳檔案的大小為 1MB
+    fileSize: 1000000,
+  },
+  storage: storagePicture,
+  fileFilter: fileFilter,
+});
+
+export { getAge, upload };
