7aeb5310340d84d130aa3b5c4cc459b7614f28eb
Meng-Tzu Tsai <e34049030@gs.ncku.edu.tw>
2023-04-29 14:35:36 +0800

    [feat] dynamically switch between sign-in & sugn-up form

--- public/render/login.js
+++ public/render/login.js
@@ -18,63 +18,106 @@ let fetchOption = {
 };
 let data = {};
 
-// 當按下 signIn button
-$("#signIn").on("submit", function (e) {
+// 從登入表單轉換成註冊表單
+$("#join").click(function (e) {
   e.preventDefault();
+  $("#title div h3").text("註冊");
+  $("#background-image").css("background-image", "url(./images/signup.jpg)");
 
-  data.inputEmail = $("#signInEmail").val();
-  data.inputPassword = $("#signInPwd").val();
+  $("#nickname").css("display", "");
+  $("#nickname").children("input").prop("required", true);
 
-  fetchOption.body = JSON.stringify(data);
+  const $submitBtn = $("#bnt-login").children("button");
+  $submitBtn.text("開始一趟新旅程！");
 
-  userApi += "signin";
+  $(".text-center")
+    .contents()
+    .filter(function () {
+      return this.nodeType == 3;
+    })
+    .first()
+    .replaceWith("已經是會員了嗎？");
 
-  // 立即執行函式
-  (async () => {
-    const userData = await getApi(userApi, fetchOption);
-    if (!userData) {
-      $(".signInErrorMsg")
-        .css("display", "inline")
-        .text("Sorry, your input is not correct.");
-
-      userApi = "/api/1.0/user/";
-    } else {
-      $(".signInErrorMsg").css("display", "none");
-      localStorage.setItem("token", userData.access_token);
-
-      // 跳轉到配對首頁
-      window.location.href = "/";
-    }
-  })();
+  $(this).css("display", "none");
+  $("#member").css("display", "inline");
 });
 
-// 當按下 signUp button
-$("#signUp").on("submit", function (e) {
+// 從註冊表單轉換成登入表單
+$("#member").click(function (e) {
+  e.preventDefault();
+  $("#title div h3").text("登入");
+  $("#background-image").css("background-image", "url(./images/signin.jpg)");
+
+  $("#nickname").css("display", "none");
+  $("#nickname").children("input").prop("required", false);
+
+  const $submitBtn = $("#bnt-login").children("button");
+  $submitBtn.text("出發吧！");
+
+  $(".text-center")
+    .contents()
+    .filter(function () {
+      return this.nodeType == 3;
+    })
+    .first()
+    .replaceWith("還不是會員嗎？");
+  // $(".text-center").text("還不是會員嗎？");
+  $(this).css("display", "none");
+  $("#join").css("display", "inline");
+});
+
+// 當按下 login button
+$("#login").on("submit", function (e) {
   e.preventDefault();
 
-  data.inputEmail = $("#signUpEmail").val();
-  data.inputPassword = $("#signUpPwd").val();
-  data.inputName = $("#signUpName").val();
+  data.inputEmail = $("#loginEmail").val();
+  data.inputPassword = $("#loginPwd").val();
 
   fetchOption.body = JSON.stringify(data);
 
-  userApi += "signup";
+  // 以是否有填暱稱來判斷是登入或註冊
+  if (!$("#loginName").val()) {
+    userApi += "signin";
 
-  // 立即執行函式
-  (async () => {
-    const userData = await getApi(userApi, fetchOption);
-    if (!userData) {
-      $(".signUpErrorMsg")
-        .css("display", "inline")
-        .text("Sorry, your input is not correct.");
+    // 立即執行函式
+    (async () => {
+      const userData = await getApi(userApi, fetchOption);
+      if (!userData) {
+        $(".signInErrorMsg")
+          .css("display", "inline")
+          .text("Sorry, your input is not correct.");
 
-      userApi = "/api/1.0/user/";
-    } else {
-      $(".signUpErrorMsg").css("display", "none");
-      localStorage.setItem("token", userData.access_token);
+        userApi = "/api/1.0/user/";
+      } else {
+        $(".signInErrorMsg").css("display", "none");
+        localStorage.setItem("token", userData.access_token);
 
-      // 跳轉到個人頁面填寫問卷
-      window.location.href = "/profile.html";
-    }
-  })();
+        // 跳轉到配對首頁
+        window.location.href = "/";
+      }
+    })();
+  } else {
+    data.inputName = $("#loginName").val();
+    fetchOption.body = JSON.stringify(data);
+
+    userApi += "signup";
+
+    // 立即執行函式
+    (async () => {
+      const userData = await getApi(userApi, fetchOption);
+      if (!userData) {
+        $(".signUpErrorMsg")
+          .css("display", "inline")
+          .text("Sorry, your input is not correct.");
+
+        userApi = "/api/1.0/user/";
+      } else {
+        $(".signUpErrorMsg").css("display", "none");
+        localStorage.setItem("token", userData.access_token);
+
+        // 跳轉到個人頁面填寫問卷
+        window.location.href = "/profile.html";
+      }
+    })();
+  }
 });
