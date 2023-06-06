64d7bd692ead98eacad1e942637fba3019b2c445
Meng-Tzu Tsai <e34049030@gs.ncku.edu.tw>
2023-06-06 17:42:04 +0800

    [feat] get user's location and mark it on the map

--- public/render/map.js
+++ public/render/map.js
@@ -29,9 +29,81 @@ const createUserOption = async (users, elementName) => {
   }
 };
 
+// FIXME: 顯示地圖中心點
+const markCenter = (userCoordinate, zoom, name) => {
+  // 更換地圖中心點為使用者位置
+  const map = L.map("map").setView(userCoordinate, zoom);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19, // 最大可放大的 scale
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  // TODO: 客製 user Maker 圖示 (更換照片來源)
+  const userIcon = L.icon({
+    iconUrl: "/test1.jpg",
+    iconSize: [42, 42],
+  });
+
+  // add marker of user position
+  const userMarker = L.marker(userCoordinate, {
+    icon: userIcon,
+  }).addTo(map);
+
+  // popup setting user
+  if (name === "台北車站") {
+    userMarker.bindPopup(`<b>台北車站</b>`).openPopup();
+  } else {
+    userMarker.bindPopup(`<b>${name}</b><br>你在這！`).openPopup();
+  }
+
+  // TODO: 客製化 display range
+  L.circle(userCoordinate, {
+    color: "#3f8aff",
+    fillColor: "#3f8aff",
+    fillOpacity: 0.5,
+    radius: 1500,
+  }).addTo(map);
+};
+
+// Function3: 顯示地圖中心位置
+const showMyLocation = (zoom, name) => {
+  if (!navigator.geolocation) {
+    Swal.fire({
+      icon: "info",
+      title: "您的瀏覽器不支援取得當前位置",
+      text: "搜尋中心預設為台北車站",
+    });
+
+    markCenter([25.0480075, 121.5170613], zoom, "台北車站");
+    return;
+  }
+
+  function success(position) {
+    const userCoordinate = [
+      position.coords.latitude,
+      position.coords.longitude,
+    ];
+
+    markCenter(userCoordinate, zoom, name);
+  }
+
+  function error() {
+    Swal.fire({
+      icon: "info",
+      title: "無法取得您的位置",
+      text: "搜尋中心預設為台北車站",
+    });
+
+    markCenter([25.0480075, 121.5170613], zoom, "台北車站");
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error);
+};
+
 // ------------------------------ 前端渲染區塊 --------------------------------
 
-// 取得所有使用者 id 和 nickname
+// TODO: 取得所有使用者 id 和 nickname (不需要下拉選單)
 const allUsersUrl = `/api/1.0/user/userslist`;
 const userIdNicknamePair = {}; // {id: nickname}
 
@@ -48,96 +120,53 @@ const userIdNicknamePair = {}; // {id: nickname}
   await createUserOption(userIdNicknamePair, "user");
 })();
 
-// ----------------------- Dispaly Map ---------------------------
+// ----------------------- Display Map ---------------------------
 $("#btnSearch").click(function (e) {
   e.preventDefault();
 
   // 取得是誰上線
   const id = +$("#users").val();
   const name = $("#users option:selected").text();
+  const zoom = 14;
 
   // 取得使用者位置
-  const userCoordinate = JSON.parse(userIdNicknamePair[id].coordinate);
-
-  // 更換地圖中心點為使用者位置
-  const zoom = 14;
-  const map = L.map("map").setView(userCoordinate, zoom);
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19, // 最大可放大的 scale
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }).addTo(map);
-
-  // TODO: 客製 school Maker 圖示 (更換照片來源)
-  const userIcon = L.icon({
-    iconUrl: "/test1.jpg",
-    iconSize: [42, 42],
-  });
-
-  // add marker of user position
-  const userMarker = L.marker(userCoordinate, {
-    icon: userIcon,
-  }).addTo(map);
-
-  // display range
-  const circle = L.circle(userCoordinate, {
-    color: "#3f8aff",
-    fillColor: "#3f8aff",
-    fillOpacity: 0.5,
-    radius: 1500,
-  }).addTo(map);
-
-  // popup setting user
-  userMarker.bindPopup(`<b>${name}</b><br>You are here !!!`).openPopup();
+  showMyLocation(zoom, name);
 
   // mark candidate position
-  (async () => {
-    // 取得所有連線者的候選人名單
-    const candidatesUrl = `/api/1.0/user/matchcandidate`;
-    const userCandidateList = await getApi(candidatesUrl);
-    const userIdCandidateIdPair = {}; // {id: candidateList}
-    userCandidateList.forEach((userObj) => {
-      userIdCandidateIdPair[userObj.id] = userObj.candidateIdList;
-    });
+  // (async () => {
+  //   // 取得所有連線者的候選人名單
+  //   const candidatesUrl = `/api/1.0/user/matchcandidate`;
+  //   const userCandidateList = await getApi(candidatesUrl);
+  //   const userIdCandidateIdPair = {}; // {id: candidateList}
+  //   userCandidateList.forEach((userObj) => {
+  //     userIdCandidateIdPair[userObj.id] = userObj.candidateIdList;
+  //   });
 
-    // 取得該連線者的候選人名單
-    const certainCandidateList = userIdCandidateIdPair[id];
-    certainCandidateList.forEach((candidateId) => {
-      const candidateName = userIdNicknamePair[candidateId].name;
-      // 取得候選人位置
-      const candidateCoordinate = JSON.parse(
-        userIdNicknamePair[candidateId].coordinate
-      );
+  //   // 取得該連線者的候選人名單
+  //   const certainCandidateList = userIdCandidateIdPair[id];
+  //   certainCandidateList.forEach((candidateId) => {
+  //     const candidateName = userIdNicknamePair[candidateId].name;
+  //     // 取得候選人位置
+  //     const candidateCoordinate = JSON.parse(
+  //       userIdNicknamePair[candidateId].coordinate
+  //     );
 
-      // add marker of user position
-      const candidateMarker = L.marker(candidateCoordinate).addTo(map);
-      // TODO: tooltip setting of candidate (更換照片來源)
-      candidateMarker
-        .bindTooltip(
-          `<b>${candidateName}</b></br><img src="/test1.jpg" style="height: 20px">`,
-          {
-            direction: "bottom", // default: auto
-            sticky: false, // true 跟著滑鼠移動。default: false
-            permanent: false, // 是滑鼠移過才出現(false)，還是一直出現(true)
-            opacity: 1.0,
-          }
-        )
-        .openTooltip();
-    });
-  })();
-
-  // 客製 school Maker 圖示
-  const school = [25.0384803, 121.5323711]; // AppWorks School
-  const schoolIcon = L.icon({
-    iconUrl:
-      "https://school.appworks.tw/wp-content/uploads/2018/09/cropped-AppWorks-School-Logo-thumb.png",
-    iconSize: [42, 42],
-  });
-
-  // add marker of school position
-  const schoolMarker = L.marker(school, {
-    icon: schoolIcon,
-  }).addTo(map);
+  //     // add marker of user position
+  //     const candidateMarker = L.marker(candidateCoordinate).addTo(map);
+  //     // TODO: tooltip setting of candidate (更換照片來源)
+  //     candidateMarker
+  //       .bindTooltip(
+  //         `<b>${candidateName}</b></br><img src="/test1.jpg" style="height: 20px">`,
+  //         {
+  //           direction: "bottom", // default: auto
+  //           sticky: false, // true 跟著滑鼠移動。default: false
+  //           permanent: false, // 是滑鼠移過才出現(false)，還是一直出現(true)
+  //           opacity: 1.0,
+  //         }
+  //       )
+  //       .openTooltip();
+  //   });
+  // })();
 });
 
 // FIXME: 取得所有人的位置
